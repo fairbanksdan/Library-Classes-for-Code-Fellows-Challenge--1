dab7a1d8b688eb18358609dc5db1103d5f992f68
Dan Fairbanks <fairbanksdan@gmail.com>
2014-04-01 22:53:17 -0700

    Code Fellows Challenge #1
    Public Library with object models for library, shelf, and book.

---
+++ Shelf.m
@@ -0,0 +1,25 @@
+//
+//  Shelf.m
+//  Library
+//
+//  Created by Daniel Fairbanks on 3/31/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import "Shelf.h"
+#import "Book.h"
+
+@implementation Shelf
+
+- (instancetype) initWithName:(NSString *)shelfName{
+    if (self = [super init])
+    {
+        self.shelfName = shelfName;
+        self.books = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+
+@end
